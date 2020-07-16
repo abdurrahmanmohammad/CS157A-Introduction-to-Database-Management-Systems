671748945db2fb5562cbd43910989a5212b5e6d0
abdurrahmanmohammad <58004923+abdurrahmanmohammad@users.noreply.github.com>
2020-07-15 22:16:18 -0700

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,24 +1,34 @@
 
+
+// Methods for administrator will be called using this class and not directly.
+// Will provide mechanisms to resolve collisions and error handling (will be covered in class)
+// Will provide mechanism for sanitizing input and output
+// Will provide built in security features such as hashing
 public class Administrator {
 
-	public static void createCourse() {
+	public static String createCourse(String department, String number, String name) {
+		return Courses.insert(department, number, name);
 
 	}
 
 	public static void createRoster() {
-
 	}
 
-	public static void deleteUser() {
+	public static String deleteUser(String department, String number) {
+		return Courses.delete(department, number);
 
 	}
-	public static void updateStudent() {
+	public static String updateStudent(String department, String number, String name) {
+		return Courses.update(department, number, name);
 
 	}
-	public static void updateInstructor() {
+	public static String updateInstructor(String username, String password, String email) {
+		return Users.update(username, password, email);
 
 	}
-	public static void deleteAdministrator() {
+	public static String deleteAdministrator(String username, String password) {
+		return Users.delete(username, password);
 
 	}
 }
+
