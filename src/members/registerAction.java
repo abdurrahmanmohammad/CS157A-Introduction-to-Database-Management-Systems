04832621374b695b866793e40d2e0410830238ae
abdurrahmanmohammad <abdurrahmanmohammad@users.noreply.github.com>
2020-07-30 22:33:49 -0700

    # WARNING: head commit changed in the meantime
    Fixed and tested login and register mechanisms. Register mechanism was
    newly created, tested, given its own pages, and was given some
    front-end.

---
+++ src/members/registerAction.java
@@ -0,0 +1,56 @@
+package members;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import members.Members;
+
+/**
+ * Servlet implementation class registerAction
+ */
+@WebServlet("/registerAction")
+public class registerAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public registerAction() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Get fields from form
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		// If any fields are empty, return back to the form
+		if (firstname.isEmpty() || lastname.isEmpty() || phone.isEmpty() || email.isEmpty() || address.isEmpty()) {
+			RequestDispatcher req = request.getRequestDispatcher("register.jsp"); // Return to the form
+			req.include(request, response);
+		} else {
+			// Add member info to DB
+			Random rand = new Random();
+			String ID = Integer.toString(rand.nextInt(1000000000)); // Generate ID (9 digits)
+			// Check if ID already exists
+			while (!Members.checkID(ID)) // Keep on generating until find an ID that DNE
+				ID = Integer.toString(rand.nextInt(1000000000)); // Generate ID (9 digits)
+			Members.insert(ID, firstname, lastname, phone, email, address); // Add member to DB
+			RequestDispatcher req = request.getRequestDispatcher("confirmation.jsp"); // Go to confirmation page
+			req.forward(request, response);
+		}
+	}
+}
